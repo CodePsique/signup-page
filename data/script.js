3a2ecedac4212adfc37503564fc980e76d78804e
Allan Almeida <almeidaa405@gmail.com>
2023-10-07 12:30:17 -0400

    add logic get image

---
+++ data/script.js
@@ -0,0 +1,38 @@
+
+
+const getUsers = async () => {
+  const allUsers = await fetch("http://localhost:3333/users", {
+    method: "GET",
+    })
+    return allUsers;
+  }
+
+  const getUserProfileImage = async (id) => {
+    const profileImage = await fetch(`http://localhost:3333/users/${id}/image`);
+    return profileImage;
+  }
+
+  const createProfileElement = (userName, userImage) => {
+    const profileElement = document.createElement('div');
+    const profileImageElement = document.createElement('img');
+    const userNameElement = document.createElement('h3');
+    
+    profileImageElement.setAttribute("src", userImage);
+    userNameElement.innerText = userName;
+    profileElement.appendChild(profileImageElement);
+    profileElement.appendChild(userNameElement);
+
+    return profileElement;
+  };
+
+  const loadUsers = async () => {
+    const container = document.getElementById('container-profiles');
+    const allUsers = await getUsers().then((response) => response.json());
+    allUsers.forEach(async (user) => {
+      const profileImage = await getUserProfileImage(user.id).then((response) => response.blob());
+      const url = window.URL || window.webkitURL;
+      const profileElement = createProfileElement(user.name, url.createObjectURL(profileImage));
+      container.appendChild(profileElement);
+    })
+  }
+  window.onload = loadUsers;
